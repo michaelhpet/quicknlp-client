139c90c000faadf28d7c29994f315033a230bb9b
Michael Peter <michaelhpet@gmail.com>
2023-09-06 18:14:02 +0100

    refactor: dub algorithms list

--- src/lib/api/getAlgorithms.ts
+++ src/lib/api/getAlgorithms.ts
@@ -8,6 +8,28 @@ export default async function getAlgorithms(): Promise<AlgoInfo[]> {
     return (await response.json()).data;
   } catch (error) {
     console.log(error);
-    return [];
+    return [
+      {
+        name: "Paraphrasing",
+        description:
+          "Paraphrase generation is the task of generating an output sentence that preserves the meaning of the input sentence with variations in word choice and grammar. Two sentences are paraphrases if their meanings are equivalent but their words and syntax are different.",
+        href: "paraphrasing",
+        aboutUrl: "",
+      },
+      {
+        name: "Grammar checking",
+        description:
+          "Grammar checking is one of the most widely used tools within natural language processing (NLP) applications. Grammar checkers check the grammatical structure of sentences based on morphological processing and syntactic processing.",
+        href: "grammar-checking",
+        aboutUrl: "",
+      },
+      {
+        name: "Summarization",
+        description:
+          "Text summarization is the problem of reducing number of sentences and words of a document without changing its meaning. There are different techniques to extract information from raw text data and use it for a summarization model. Overall, they can be categorized as Extractive and Abstractive.",
+        href: "summarization",
+        aboutUrl: "",
+      },
+    ];
   }
 }
